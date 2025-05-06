166b027f7d1b9bfd10d6607ff691504925fa4dd7
22278rp <22278rp@hvhs.school.nz>
2025-05-06 01:30:42 +0000

    Task 6 completed working on task 7

--- fb_io.mjs
+++ fb_io.mjs
@@ -1,7 +1,7 @@
 //**************************************************************/
 // fb_io.mjs
 // Generalised firebase routines
-// Written by <Your Name Here>, Term 2 202?
+// Written by Ryan Parks, Term 2 2025
 //
 // All variables & function begin with fb_  all const with FB_
 // Diagnostic code lines have a comment appended to them //DIAG
@@ -23,12 +23,16 @@ import { getAuth, GoogleAuthProvider, signInWithPopup }
     from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 import { onAuthStateChanged }
     from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+import { signOut }
+    from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+import { ref, set }
+    from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
 
 /**************************************************************/
 // EXPORT FUNCTIONS
 // List all the functions called by code or html outside of this module
 /**************************************************************/
-export { fb_initialise, fb_authenticate, fb_detectLoginChange }
+export { fb_initialise, fb_authenticate, fb_detectLoginChange, fb_logout, fb_WriteRec }
 
 function fb_initialise() {
     console.log('%c fb_initialise(): ', 'color: ' + COL_C + '; background-color: ' + COL_B + ';');
@@ -49,6 +53,7 @@ function fb_initialise() {
     console.info(firebaseGameDB);
     document.getElementById("p_fbInitialise").innerHTML = "Initialised";
 }
+
 function fb_authenticate() {
     console.log('%c fb_authenticate(): ', 'color: ' + COL_C + '; background-color: ' + COL_B + ';');
     const AUTH = getAuth();
@@ -95,7 +100,50 @@ function fb_detectLoginChange() {
     });
     document.getElementById("p_fbdetectLoginChange").innerHTML = "Login change detected";
 }
+function fb_logout() {
+    console.log('%c fb_logout(): ', 'color: ' + COL_C + '; background-color: ' + COL_B + ';');
+    const AUTH = getAuth();
 
+    signOut(AUTH).then(() => {
+
+        //✅ Code for a successful logout goes here
+
+    })
+
+        .catch((error) => {
+
+            //❌ Code for a logout error goes here
+
+        });
+    document.getElementById("p_fblogout").innerHTML = "Logged out";
+}
+
+function fb_WriteRec() {
+    console.log('%c fb_WriteRec(): ', 'color: ' + COL_C + '; background-color: ' + COL_B + ';');
+    const DB = getDatabase()
+    const ref = ref(DB, "/UserData");
+
+    set(ref,
+        {
+            apiKey: "AIzaSyAQ3Qc6Ej_4YvNXCAjqsfLoA8p75j3R7-8",
+            authDomain: "comp2025-ryan-parks.firebaseapp.com",
+            databaseURL: "https://comp2025-ryan-parks-default-rtdb.asia-southeast1.firebasedatabase.app",
+            projectId: "comp2025-ryan-parks",
+            storageBucket: "comp2025-ryan-parks.firebasestorage.app",
+            messagingSenderId: "73072219046",
+            appId: "1:73072219046:web:7608445213a3fd3e973567",
+            measurementId: "G-R89L1J8Z4D"
+        }).then(() => {
+
+            //✅ Code for a successful write goes here
+
+        }).catch((error) => {
+
+            //❌ Code for a write error goes here
+
+        });
+    document.getElementById("p_fbWriteRec").innerHTML = "record written";
+}
 
 /**************************************************************/
 // END OF CODE
--- main.mjs
+++ main.mjs
@@ -5,7 +5,7 @@
 /**************************************************************/
 const COL_C = 'white';	    // These two const are part of the coloured 	
 const COL_B = '#CD7F32';	//  console.log for functions scheme
-console.log('%c main.mjs', 
+console.log('%c main.mjs',
     'color: blue; background-color: white;');
 
 /**************************************************************/
@@ -14,13 +14,20 @@ console.log('%c main.mjs',
 // Import all the constants & functions required from fb_io module
 import { fb_initialise }
     from './fb_io.mjs';
-    window.fb_initialise   = fb_initialise;
+window.fb_initialise = fb_initialise;
 import { fb_authenticate }
     from './fb_io.mjs';
-    window.fb_authenticate   = fb_authenticate;
+window.fb_authenticate = fb_authenticate;
 import { fb_detectLoginChange }
     from './fb_io.mjs';
-    window.fb_detectLoginChange = fb_detectLoginChange;
+window.fb_detectLoginChange = fb_detectLoginChange;
+import { fb_logout }
+    from './fb_io.mjs';
+window.fb_logout = fb_logout;
+import { fb_WriteRec }
+    from './fb_io.mjs';
+window.fb_WriteRec = fb_WriteRec;
+
 
 
 
